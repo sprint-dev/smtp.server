c07728c7f0a2c932dce6cfc26faa0dd129897706
Paul <paul.petre.online@gmail.com>
2025-02-17 14:20:28 +0100

    Add files via upload

--- Services/SmtpServiceFail.cs
+++ Services/SmtpServiceFail.cs
@@ -1,14 +1,16 @@
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using log4net;
+using log4net.Util;
 
-public class SmtpService {
+public class SmtpServiceFail {
     private readonly ILog log;
     private readonly TcpClient client;
     private int emailNo = 0;
     private int threadNo = 0;
 
-    public SmtpService(TcpClient client, ILog log, int threadNo) {
+    public SmtpServiceFail(TcpClient client, ILog log, int threadNo) {
         this.log = log;
         this.client = client;
         this.threadNo = threadNo;
@@ -59,7 +61,7 @@ public class SmtpService {
                 continue;
             }
 
-            if (strMessage.StartsWith("MAIL FROM") || strMessage.Contains("MAIL FROM")) {                
+            if (strMessage.StartsWith("MAIL FROM") || strMessage.Contains("MAIL FROM")) {
                 emailNo++;
                 log.Info($"MESSAGE MAIL FROM, {threadNo}");
                 Write("250 OK");
@@ -70,11 +72,17 @@ public class SmtpService {
                 log.Info($"MESSAGE DATA, {threadNo}");
                 Write("354 Start mail input; end with");
                 strMessage = Read();
+                if (threadNo > 10 && threadNo%11 == 0) {
+                    Thread.Sleep(1000);
+                    log.Info($"TIMEOUT, {strMessage},  {threadNo}");
+                    Write("421 4.4.1 Connection timed out. Total session duration: 00:10:01.8904373");
+                    //Thread.Sleep(100000);
+                    break;
+                }
                 log.Info($"MESSAGE BODY, {strMessage},  {threadNo}");
                 Write("250 OK");
                 continue;
             }
-
 
             if (strMessage.Length != 0) {
                 log.Info($"MESSAGE BODY, {strMessage},  {threadNo}");
@@ -83,19 +91,27 @@ public class SmtpService {
     }
 
     private void Write(string strMessage) {
-        NetworkStream clientStream = client.GetStream();
-        ASCIIEncoding encoder = new();
-        byte[] buffer = encoder.GetBytes(strMessage + "\r\n");
-        clientStream.Write(buffer, 0, buffer.Length);
-        clientStream.Flush();
+        try {
+            NetworkStream clientStream = client.GetStream();
+            ASCIIEncoding encoder = new();
+            byte[] buffer = encoder.GetBytes(strMessage + "\r\n");
+            clientStream.Write(buffer, 0, buffer.Length);
+            clientStream.Flush();
+        } catch (Exception ex) {
+            string mess = ex.Message;
+        }
     }
 
     private string Read() {
-        byte[] messageBytes = new byte[8192];
-        NetworkStream clientStream = client.GetStream();
-        ASCIIEncoding encoder = new();
-        int bytesRead = clientStream.Read(messageBytes, 0, 8192);
-        string strMessage = encoder.GetString(messageBytes, 0, bytesRead);
-        return strMessage;
+        try {
+            byte[] messageBytes = new byte[8192];
+            NetworkStream clientStream = client.GetStream();
+            ASCIIEncoding encoder = new();
+            int bytesRead = clientStream.Read(messageBytes, 0, 8192);
+            string strMessage = encoder.GetString(messageBytes, 0, bytesRead);
+            return strMessage;
+        } catch (Exception ex) {
+            return ex.Message;
+        }
     }
 }
